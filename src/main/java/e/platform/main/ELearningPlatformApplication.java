41df8511c490053ceed054bda9d6e1039bddaea1
omar deeb <thephroglet>
2025-02-27 18:30:25 -0800

    first commit

---
+++ src/main/java/e/platform/main/ELearningPlatformApplication.java
@@ -0,0 +1,13 @@
+package e.platform.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ELearningPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ELearningPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/e/platform/main/ELearningPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package e.platform.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ELearningPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
